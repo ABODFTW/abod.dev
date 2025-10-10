a93c72e6b2d33bf549e0e9989cf04bbabec02573
Abod <hello@abod.dev>
2025-10-10 16:17:57 +0300

    Update NotionPage component and site configuration
    - Commented out the GitHubShareButton in NotionPage.tsx
    - Changed rootNotionPageId to a new value
    - Updated site name, domain, and author to reflect personal branding
    - Modified description and social usernames for consistency with new branding

--- site.config.ts
+++ site.config.ts
@@ -2,24 +2,23 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1a3ed18eef1f4074a0352d16a607fe1b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Abod\'s Blog',
+  domain: 'abod.dev',
+  author: 'Abod',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Abod\'s personal blog',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'abod_ftw',
+  github: 'abodftw',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
